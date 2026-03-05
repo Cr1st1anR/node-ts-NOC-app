cede6dc51d2e3de1e7df98630ec99bbd2b733148
Cristhian Rengifo <cristhian860@hotmail.com>
2026-03-05 18:38:15 -0500

    feat: agregar pruebas y reportes de cobertura para envs.plugin.ts

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -3,6 +3,7 @@ import * as env from 'env-var';
 
 
 export const envs = {
+    PORT: env.get('PORT').required().asPortNumber(),
     MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
@@ -15,9 +16,9 @@ export const envs = {
     MONGO_PASS: env.get('MONGO_PASS').required().asString(),
 
     //Postgres
-    POSTGRES_URL: env.get('POSTGRES_URL').required().asString(),
-    POSTGRES_USER: env.get('POSTGRES_USER').required().asString(),
-    POSTGRES_DB: env.get('POSTGRES_DB').required().asString(),
-    POSTGRES_PASSWORD: env.get('POSTGRES_PASSWORD').required().asString(),
+    // POSTGRES_URL: env.get('POSTGRES_URL').required().asString(),
+    // POSTGRES_USER: env.get('POSTGRES_USER').required().asString(),
+    // POSTGRES_DB: env.get('POSTGRES_DB').required().asString(),
+    // POSTGRES_PASSWORD: env.get('POSTGRES_PASSWORD').required().asString(),
 
 }
