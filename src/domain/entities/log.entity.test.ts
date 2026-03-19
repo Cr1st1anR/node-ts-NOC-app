51fa6456e38dbb839e5358d28a9ec607413e3d9c
Cristhian Rengifo <cristhian860@hotmail.com>
2026-03-18 23:28:10 -0500

    Add log entity and check service with tests
    - Implement LogEntity class with methods for creating instances from JSON and objects.
    - Create CheckService class for checking service availability and logging results.
    - Add tests for LogEntity to verify instance creation and methods.
    - Add tests for CheckService to ensure correct behavior on success and error cases.
    - Generate coverage reports for log.entity.ts and check-service.ts.
    - Create mock data source for testing log operations.

---
+++ src/domain/entities/log.entity.test.ts
@@ -0,0 +1,51 @@
+import { LogEntity, LogSeverityLevel } from "./log.entity";
+
+
+describe('log.entity.ts LogEntity', () => {
+
+    const dataObj = {
+        message: 'Hola Mundo',
+        level: LogSeverityLevel.high,
+        origin: 'log.entity.test.ts'
+    }
+
+    test('should creat a LogEntity instance', () => {
+
+
+        const log = new LogEntity(dataObj);
+
+        expect(log).toBeInstanceOf(LogEntity);
+        expect(log.message).toBe(dataObj.message);
+        expect(log.level).toBe(dataObj.level);
+        expect(log.origin).toBe(dataObj.origin);
+        expect(log.createdAt).toBeInstanceOf(Date);
+    });
+
+    test('should create a LogEntity instance from json', () => {
+
+        const json = `{"message":"Service https://google.com working","level":"low","createdAt":"2026-03-05T04:04:25.682Z","origin":"check-service.ts"}`;
+
+        const log = LogEntity.fromJson(json);
+
+
+        expect(log).toBeInstanceOf(LogEntity);
+        expect(log.message).toBe("Service https://google.com working");
+        expect(log.level).toBe(LogSeverityLevel.low);
+        expect(log.origin).toBe("check-service.ts");
+        expect(log.createdAt).toBeInstanceOf(Date);
+
+
+    });
+
+    test('should create a LogEntity instance from object', () => {
+
+        const log = LogEntity.fromObject(dataObj);
+
+        expect(log).toBeInstanceOf(LogEntity);
+        expect(log.message).toBe(dataObj.message);
+        expect(log.level).toBe(dataObj.level);
+        expect(log.origin).toBe(dataObj.origin);
+        expect(log.createdAt).toBeInstanceOf(Date);
+
+    })
+})
