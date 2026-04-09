85c4c0fd221e9963222c676b65a0c87569232735
Cristhian Rengifo <cristhian860@hotmail.com>
2026-04-09 12:41:48 -0500

    feat: test CronService

--- src/presentation/cron/cron-service.ts
+++ src/presentation/cron/cron-service.ts
@@ -6,13 +6,13 @@ type OnTick = () => void;
 
 //Patron adaptador que adapta nuestro paquete de terceros (cron)
 export class CronService {
-    
+
     static createJob(cronTime: CronTime, onTick: OnTick): CronJob {
 
         const job = new CronJob(cronTime, onTick);
-                
-                job.start() ;
 
-                return job;
+        job.start();
+
+        return job;
     }
 }
